9a21faa0c07e441890aaf71af67dd678dd0cd34f
jennahyasin <yasinjennah@gmail.com>
2023-04-14 00:50:04 -0700

    added all up to date course info

---
+++ script.js
@@ -0,0 +1,60 @@
+document.getElementById("300").addEventListener("click", myFunction1);
+      function myFunction1() {
+        window.location.href="/300.html";
+      }
+
+      document.getElementById("462S").addEventListener("click", myFunction2);
+      function myFunction2() {
+        window.location.href="/462S.html";
+      }
+      document.getElementById("338").addEventListener("click", myFunction3);
+      function myFunction3() {
+        window.location.href="/338.html";
+      }
+      document.getElementById("363").addEventListener("click", myFunction4);
+      function myFunction4() {
+        window.location.href="/363.html";
+      }
+      document.getElementById("334").addEventListener("click", myFunction5);
+      function myFunction5() {
+        window.location.href="/334.html";
+      }
+      document.getElementById("311").addEventListener("click", myFunction6);
+      function myFunction6() {
+        window.location.href="/311.html";
+      }
+      document.getElementById("336").addEventListener("click", myFunction7);
+      function myFunction7() {
+        window.location.href="/336.html";
+      }
+
+      document.getElementById("328").addEventListener("click", myFunction8);
+      function myFunction8() {
+        window.location.href="/328.html";
+      }
+
+      document.getElementById("370").addEventListener("click", myFunction9);
+      function myFunction9() {
+        window.location.href="/370.html";
+      }
+
+      document.getElementById("383").addEventListener("click", myFunction10);
+      function myFunction10() {
+        window.location.href="/383.html";
+      }
+      document.getElementById("438").addEventListener("click", myFunction11);
+      function myFunction11() {
+        window.location.href="/438.html";
+      }
+      document.getElementById("329").addEventListener("click", myFunction12);
+      function myFunction12() {
+        window.location.href="/329.html";
+      }
+      document.getElementById("489").addEventListener("click", myFunction13);
+      function myFunction13() {
+        window.location.href="/489.html";
+      }
+      document.getElementById("499").addEventListener("click", myFunction14);
+      function myFunction14() {
+        window.location.href="/499.html";
+      }
